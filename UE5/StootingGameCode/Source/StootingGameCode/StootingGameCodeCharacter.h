f9a11c449acf088743483ed67b4c3daa10163bc4
wontae <83wontae@gmail.com>
2023-11-01 12:15:38 +0900

    ...

---
+++ UE5/StootingGameCode/Source/StootingGameCode/StootingGameCodeCharacter.h
@@ -0,0 +1,87 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "StootingGameCodeCharacter.generated.h"
+
+
+UCLASS(config=Game)
+class AStootingGameCodeCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+	
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+
+	/** Test Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* TestAction;
+
+public:
+	AStootingGameCodeCharacter();
+	
+
+protected:
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** Called for Test input */
+	void Test(const FInputActionValue& Value);
+			
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// To add mapping context
+	virtual void BeginPlay();
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+public:
+	//Server: 서버에서 실행
+	//Reliable: 신뢰성
+	UFUNCTION(Server, Reliable)
+	void ReqTestMsg();
+
+	//NetMulticast: 모두에서 실행(클라이언트와 서버)
+	UFUNCTION(NetMulticast, Reliable)
+	void ResTestMsg();
+
+	//Client: 서버가 소유중인 클라이언트에서 실행
+	UFUNCTION(Client, Reliable)
+	void ResTestMsgToOwner();
+};
+
---
+++ UE5/StootingGameCode/Source/StootingGameCode/StootingGameCodeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StootingGameCodeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStootingGameCodeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStootingGameCodeGameMode();
+};
+
+
+
